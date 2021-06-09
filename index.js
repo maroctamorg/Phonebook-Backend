d2bbd19bd629148f7544d2b2d70c55a0297e6761
maroctamorg <68016600+maroctamorg@users.noreply.github.com>
2021-06-09 11:39:41 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const { response } = require("express")
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+
+
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
