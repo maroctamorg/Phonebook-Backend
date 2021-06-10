5ac85a4b35babf756eb1487e2c544fb61330b662
maroctamorg <68016600+maroctamorg@users.noreply.github.com>
2021-06-10 10:11:01 +0200

    Implemented exercises 3.1->3.8 in phonebook_backend

--- index.js
+++ index.js
@@ -1,12 +1,117 @@
-const { response } = require("express")
+const { response, request } = require("express")
 const express = require("express")
 const app = express()
 
+const morgan = require('morgan')
+
 app.use(express.json())
 
+//app.use(morgan('tiny'))
+morgan.token('content', function (req, res) { return( JSON.stringify(req.body) ) } )
+app.use(morgan( (tokens, req, res) => {
+    if(req.method == "POST") {
+        return[
+            tokens.method(req, res),
+            tokens.url(req, res),
+            tokens.status(req, res),
+            tokens.res(req, res, 'content-length'), '-',
+            tokens['response-time'](req, res), 'ms',
+            tokens['content'](req, res)
+        ].join(' ')
+    } else {
+        return [
+            tokens.method(req, res),
+            tokens.url(req, res),
+            tokens.status(req, res),
+            tokens.res(req, res, 'content-length'), '-',
+            tokens['response-time'](req, res), 'ms',
+        ].join(' ')
+    }
+}))
 
+let phonebook = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+    }
+]
 
+app.get('/info', (request, response) => {
+    response.send(`
+    <p>Phonebook has info for ${phonebook.length} people</p>
+    <p>${Date()}</p>
+    `)
+})
 
+app.get('/api/persons', (request, response) => {
+    response.json(phonebook)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const entry = phonebook.find( entry => entry.id === id )
+
+    if(entry) {
+        response.json(entry)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    phonebook = phonebook.filter( entry => entry.id !== id)
+    response.status(204).end()
+})
+
+const generateId = (max) => {
+    return Math.floor( Math.random() * max )
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if(!body.name) {
+        return response.status(400).json({
+            error: "name missing"
+        })
+    } else if(!body.number) {
+        return response.status(400).json({
+            error: "number missing"
+        })
+    }
+
+    if(phonebook.find( entry => entry.name === body.name )) {
+        return response.status(400).json({
+            error: `entry already exists for ${body.name}`
+        })
+    }
+
+    const entry = {
+        name: body.name,
+        number: body.number,
+        id: generateId(9999999)
+    }
+
+    phonebook = phonebook.concat(entry)
+    response.json(entry)
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
